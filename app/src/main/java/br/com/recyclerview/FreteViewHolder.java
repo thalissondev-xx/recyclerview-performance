5294460f4f090b31129d2aed38ffff24530bd272
Thalisson <thalissondev@gmail.com>
2016-03-14 22:46:19 -0300

    Test click

--- app/src/main/java/br/com/recyclerview/FreteViewHolder.java
+++ app/src/main/java/br/com/recyclerview/FreteViewHolder.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by thalissonestrela on 14/03/16.
  */
-public class FreteViewHolder extends RecyclerView.ViewHolder {
+public class FreteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private Context context;
     public TextView tvOrigem;
     public TextView tvDestino;
@@ -18,5 +19,10 @@ public class FreteViewHolder extends RecyclerView.ViewHolder {
         this.context = context;
         this.tvOrigem = (TextView) view.findViewById(R.id.tvOrigem);
         this.tvDestino = (TextView) view.findViewById(R.id.tvDestino);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this.context, "" + getPosition(), Toast.LENGTH_LONG).show();
     }
 }
